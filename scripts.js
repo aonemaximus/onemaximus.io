d504aa2c1c8fd479fcbd626da3e144d4af2c7105
aonemaximus <maximusalpha41@gmail.com>
2025-04-30 11:07:10 +0530

    Update and rename scripts.js to script.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-// GSAP Animations for smooth transitions
-gsap.from(".hero h1", { opacity: 0, y: -100, duration: 1 });
-gsap.from(".hero p", { opacity: 0, y: 100, duration: 1, delay: 0.5 });
-gsap.from(".navbar", { opacity: 0, y: -100, duration: 1, delay: 1 });
